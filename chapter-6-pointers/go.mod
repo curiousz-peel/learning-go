218e5613fc360aca97c68f942c43342dc51cc806
George Dan Zbutea <george.zbutea@grabtaxi.com>
2024-07-05 10:01:00 +0300

    chapter six ✅

---
+++ chapter-6-pointers/go.mod
@@ -0,0 +1,3 @@
+module chapter-6-pointers
+
+go 1.22.0
---
+++ chapter-6-pointers/main.go
@@ -0,0 +1,29 @@
+package main
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func MakePerson(firstName, lastName string, age int) Person {
+	return Person{
+		FirstName: firstName,
+		LastName:  lastName,
+		Age:       age,
+	}
+}
+
+func MakePersonPointer(firstName, lastName string, age int) *Person {
+	return &Person{
+		FirstName: firstName,
+		LastName:  lastName,
+		Age:       age,
+	}
+}
+
+func main() {
+	_ = MakePerson("Dave", "Grohl", 48)
+
+	_ = MakePersonPointer("Bob", "Irwin", 52)
+}
