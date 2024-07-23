a7067d255d0456f491f53c574592a06fc36012e1
George Dan Zbutea <george.zbutea@grabtaxi.com>
2024-07-23 15:57:53 +0300

    changes for v2.0.0, added generics support

--- chapter-10-modules/modules.go
+++ chapter-10-modules/modules.go
@@ -1,8 +1,14 @@
 package chapter10modules
 
-// Add [adds] two integers
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add [adds] two numbers of type [constraints.Integer] or [constraints.Float]
 //
 // [adds] : https://www.mathsisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
